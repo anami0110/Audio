54df2db6e798100f3d6d1ab746318d1dc64d664e
anami0110 <124621040+anami0110@users.noreply.github.com>
2023-04-13 20:54:16 +0200

    l

--- graba_audio.py
+++ graba_audio.py
@@ -8,58 +8,49 @@ Created on Thu Apr 13 20:21:53 2023
 import pyaudio
 import wave
 
-# Configuración de la grabación
+# Configuración de la grabación de audio
+CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 16000
-CHUNK = 1024
+RATE = 44100
 RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "grabacion.wav"
+OUTPUT_FILENAME = "grabacion.wav"
+DEVICE_INDEX = 0  # Índice del dispositivo de hardware (0,0)
 
 # Inicializar PyAudio
-audio = pyaudio.PyAudio()
+p = pyaudio.PyAudio()
 
-# Obtener el índice del micrófono en el puerto jack de 3.5 mm
-mic_index = None
-for i in range(audio.get_device_count()):
-    info = audio.get_device_info_by_index(i)
-    if "3.5mm" in info["name"]:
-        mic_index = info["index"]
-        break
-
-# Verificar si se encontró el micrófono
-if mic_index is None:
-    print("No se encontró un micrófono en el puerto jack de 3.5 mm.")
-    audio.terminate()
-    exit(1)
-
-
-# Abrir un flujo de audio para la grabación
-stream = audio.open(format=FORMAT, channels=CHANNELS,
-                rate=RATE, input=True,
-                frames_per_buffer=CHUNK)
+# Abrir el flujo de audio desde el dispositivo de hardware específico
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK,
+                input_device_index=DEVICE_INDEX)
 
 print("Grabando audio...")
+
 frames = []
 
-# Grabar audio durante 5 segundos
+# Grabar audio en trozos
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
     frames.append(data)
 
 print("Grabación completada.")
 
-# Detener y cerrar el flujo de audio
+# Detener el flujo de audio
 stream.stop_stream()
 stream.close()
-audio.terminate()
+p.terminate()
 
 # Guardar la grabación en un archivo WAV
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf = wave.open(OUTPUT_FILENAME, 'wb')
 wf.setnchannels(CHANNELS)
-wf.setsampwidth(audio.get_sample_size(FORMAT))
+wf.setsampwidth(p.get_sample_size(FORMAT))
 wf.setframerate(RATE)
 wf.writeframes(b''.join(frames))
 wf.close()
 
-print("Grabación guardada en: {}".format(WAVE_OUTPUT_FILENAME))
+print(f"Grabación guardada en {OUTPUT_FILENAME}")
+
