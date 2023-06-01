a2213b01a451f73406cda97c93a35bc3a393bc89
anami0110 <124621040+anami0110@users.noreply.github.com>
2023-06-01 13:18:23 +0200

    Update gps2.py

--- gps2.py
+++ gps2.py
@@ -10,10 +10,10 @@ def get_location():
     ip_address = get_ip()
     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
     location_data = {
-        "ip": ip_address,
-        "city": response.get("city"),
-        "region": response.get("region"),
-        "country": response.get("country_name")
+        IP: ip_address,
+        Ciudad: response.get("city"),
+        Región: response.get("region"),
+        País: response.get("country_name")
     }
     return location_data
 
