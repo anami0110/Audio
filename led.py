7d636c900be417ef83e669699af873564eaf2f74
anami0110 <124621040+anami0110@users.noreply.github.com>
2023-06-07 19:23:45 +0200

    Update led.py

--- led.py
+++ led.py
@@ -1,10 +1,26 @@
 from periphery import GPIO
 import time
 
-led = GPIO("/dev/gpiochip0", 6, "out")  # pin 13
+rojo = GPIO("/dev/gpiochip0", 6, "out")  # pin 13
+azul = GPIO("/dev/gpiochip2", 9, "out")  # pin 16
+verde = GPIO("/dev/gpiochip4", 10, "out")  # pin 18
+blanco = GPIO("/dev/gpiochip4", 12, "out")  # pin 22
+amarillo = GPIO("/dev/gpiochip0", 7, "out")  # pin 29
 
-led.write(True)
+rojo.write(True)
+azul.write(True)
+verde.write(True)
+blanco.write(True)
+amarillo.write(True)
 time.sleep(5)
-led.write(False)
-led.close()
+rojo.write(False)
+azul.write(False)
+verde.write(False)
+blanco.write(False)
+amarillo.write(False)
+rojo.close()
+verde.close()
+azul.close()
+blanco.close()
+amarillo.close()
 	
