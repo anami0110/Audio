3a8117d1c61a8af1e2da35051d6d4625b6190334
anami0110 <124621040+anami0110@users.noreply.github.com>
2023-06-19 21:40:24 +0200

    Create Contador.py

---
+++ Contador.py
@@ -0,0 +1,29 @@
+import time
+
+def contador_tiempo():
+    # Establecer el tiempo límite en segundos
+    tiempo_limite = 600  # 10 minutos
+
+    # Obtener el tiempo de inicio
+    tiempo_inicio = time.time()
+
+    while True:
+        # Verificar si se ha excedido el tiempo límite
+        if time.time() - tiempo_inicio > tiempo_limite:
+            print("Tiempo límite alcanzado. ¡Saliendo del programa!")
+            break
+
+        # Leer la entrada del usuario
+        entrada = input("Ingresa un carácter: ")
+
+        # Verificar si no se ha ingresado ningún carácter
+        if not entrada:
+            print("No se ingresó ningún carácter. ¡Saliendo del programa!")
+            break
+
+        # Si se ingresó un carácter, continuar con la ejecución del programa
+        # ...
+        # Aquí puedes poner el resto de tu lógica
+
+# Llamar a la función para iniciar el contador de tiempo
+contador_tiempo()
